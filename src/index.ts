81a6bc76e36ab7271aabe1366e74239d9c0f9ddd
Bharathi <nikg4u@gmail.com>
2019-10-24 18:09:40 +0530

    Major part of lobby is over

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ import PIXI = require("pixi.js");
 import { Context, MVCSBundle } from "@robotlegsjs/core";
 import { ContextView, PixiBundle } from "@robotlegsjs/pixi";
 import { GameConfig } from "./config/GameConfig";
-import * as SFS2X from "sfs2x-api";
+//import * as SFS2X from "sfs2x-api";
 import "reflect-metadata";
-import * as express from 'express';
-import * as bodyParser from "body-parser";
+//import * as express from 'express';
+//import * as bodyParser from "body-parser";
 const fetch = require('node-fetch');
 
 export class Game {
   private _app: PIXI.Application;
   private context: Context;
   private gameConfig:GameConfig;
+  private gameWindow;
 
   constructor() {
     this._app = new PIXI.Application({
@@ -56,6 +57,37 @@ export class Game {
     this._app.renderer.render(this._app.stage);
     window.requestAnimationFrame(this.render);
   };
+
+  public OpenWindow(url:string)
+  {
+    console.log("OpenWindow url  = " + url);
+    if(this.gameWindow == null)
+    {
+      this.gameWindow = window.open(url, '_blank', 'height=1050,width=1050,menubar=1,resizable=0');
+    }
+    else{
+      if(!this.gameWindow.closed)
+      {
+        this.gameWindow.close();
+      }
+      this.gameWindow = window.open(url, '_blank', 'height=1050,width=1050,menubar=1,resizable=0');
+      /*try
+      {
+        this.gameWindow.open(url, '_blank', 'height=1050,width=1050,menubar=1,resizable=0');
+      }
+      catch (Error) 
+      {
+        alert(Error.message);  
+        this.gameWindow = window.open(url, '_blank', 'height=1050,width=1050,menubar=1,resizable=0');
+      }
+      finally
+      {
+        //this.gameWindow = window.open(url, '_blank', 'height=1050,width=1050,menubar=1,resizable=0');
+      }*/
+      //finally{}
+     
+    }
+  }
 }
 
 export const game: Game = new Game();
--- src/views/GameView.ts
+++ src/views/GameView.ts
@@ -2,6 +2,7 @@ import { Game } from './../index';
 import * as global from  './../index';
 import { Container, Sprite} from "pixi.js";
 import { Games } from './../entity/Games';
+import { object } from 'prop-types';
 
 
   export class GameView extends Container {
@@ -23,7 +24,10 @@ import { Games } from './../entity/Games';
     private app;
     private dealspr;
     private lobbytable;
+    private home;
     private container;
+    private DataGrid;
+    private nOpenGame:number = 0;
 
     constructor() {
     super();
@@ -88,18 +92,27 @@ import { Games } from './../entity/Games';
     cashspr.y = 170;
     cashspr.scale.x = 0.67;
     cashspr.scale.y = 0.67;
+    cashspr.interactive = true;
+    cashspr.buttonMode = true;
+    cashspr.on('pointerdown', this.onCash, this);
 
     const tournamentspr = new PIXI.Sprite(this.tournament);
     tournamentspr.x = 0;
     tournamentspr.y = 317;
     tournamentspr.scale.x = 0.67;
     tournamentspr.scale.y = 0.67;
+    tournamentspr.interactive = true;
+    tournamentspr.buttonMode = true;
+    tournamentspr.on('pointerdown', this.onTournament, this);
 
     const practicespr = new PIXI.Sprite(this.practice);
     practicespr.x = 0;
     practicespr.y = 464;
     practicespr.scale.x = 0.67;
     practicespr.scale.y = 0.67;
+    practicespr.interactive = true;
+    practicespr.buttonMode = true;
+    practicespr.on('pointerdown', this.onPractice, this);
     /*this.crown = PIXI.Texture.from("./../src/Assets/img/cash.png");
     const crownspr = new PIXI.Sprite(this.cash);
     crownspr.x = 0;
@@ -112,6 +125,7 @@ import { Games } from './../entity/Games';
     addCashspr.y = 13;// + 10;
     addCashspr.scale.x = 0.087;
     addCashspr.scale.y = 0.087;
+    
     this.funCoins = PIXI.Texture.from("./../src/Assets/img/fun_coins.png");
     const funCoinsspr = new PIXI.Sprite(this.funCoins);
     funCoinsspr.x = 1124;
@@ -150,6 +164,21 @@ import { Games } from './../entity/Games';
     //this.app.render(this.bg);
   }
 
+  private onTournament(event)
+  {
+    this.strcurrencyType = "tournament";
+  }
+
+  private onPractice(event)
+  {
+    this.strcurrencyType = "practice";
+  }
+
+  private onCash(event)
+  {
+    this.strcurrencyType = "cash";
+  }
+
   private onClick(evt)
   {
     console.log("onClick evt = " + Object.getOwnPropertyNames(evt));
@@ -164,47 +193,73 @@ import { Games } from './../entity/Games';
     console.log("onClick this  = " + this.name);
     console.log("onClick currentTarget = " + evt.currentTarget.name);
     console.log("onClick target = " + evt.target.name);
+    var arrHeaderText:string[] = [];
+    var arrDataGridHeaderText:string[] = [];
+    arrHeaderText = ['My Account', 'Promotions', 'Bring-A-Friend', 'RPS'];
     if(evt.target.name == "deal")
     {
-      this.RenderLobbyTables(global.game.getGameConfig().getlobbyData(evt.target.name, this.strcurrencyType));
+      arrDataGridHeaderText = ['Name','Deals','Max Players','Entry Fee','Prize','Active Players','Registering','Action']
+      this.RenderLobbyTables(global.game.getGameConfig().getlobbyData(evt.target.name, this.strcurrencyType), "Deal", arrHeaderText,arrDataGridHeaderText);
       //console.log("gottttttt******if(evt == this.dealspr)***************** it ");
     }
     if(evt.target.name == "pool")
     {
-      this.RenderLobbyTables(global.game.getGameConfig().getlobbyData(evt.target.name, this.strcurrencyType));
+      arrDataGridHeaderText = ['Name','Type','Max Players','Entry Fee','Prize','Active Players','Registering','Action']
+      this.RenderLobbyTables(global.game.getGameConfig().getlobbyData(evt.target.name, this.strcurrencyType), "Pool", arrHeaderText,arrDataGridHeaderText);
       //console.log("gottttttt**********if(evt.currentTarget == this.dealspr)************* it ");
     }
     if(evt.target.name == "point")
     {
-      this.RenderLobbyTables(global.game.getGameConfig().getlobbyData(evt.target.name, this.strcurrencyType));
+      arrDataGridHeaderText = ['Name','Decks','Max Players','Min- Entry','Prize','Active Players','Registering','Action']
+      this.RenderLobbyTables(global.game.getGameConfig().getlobbyData(evt.target.name, this.strcurrencyType), "Point",arrHeaderText,arrDataGridHeaderText);
       //console.log("gottttttt********if(evt.target == this.dealspr)*************** it ");
     }
   }
 
-  private RenderLobbyTables(param)
+  private RenderLobbyTables(param, strType:string, arrHeader:string[] = [], arrDataGridHeader:string[] = [])
   {
     //this.lobbytable = PIXI.Texture.from("./../src/Assets/img/Lobby-Table.jpg");
-    
+    console.log("Lobby exist?" + this.container.getChildByName(strType+"-"+this.strcurrencyType));
+    const startLobby = this.container.getChildByName("Start");
+    startLobby.visible = false;
+    if(this.container.getChildByName(strType) != null)
+    {
+      this.DataGrid = this.container.getChildByName(strType);
+      this.DataGrid.visible = true;
+      const home = this.container.getChildByName("home");
+      home.visible = false
+      return;
+    }
     const DataGrid = new PIXI.Container();
+    DataGrid.name = strType+"-"+this.strcurrencyType;
     const Header = new PIXI.Graphics();
     Header.beginFill(0x0c0015);//(0x0c0015);(0xDE3249)
     Header.drawRect(60, 137, 1159, 102);
     Header.endFill();
     Header.lineStyle(2, 0xFF00FF, 1);
+    
 
     const HeaderH = new PIXI.Graphics();
-    HeaderH.beginFill(0x210237, 0.5);//0x650A5A 0x210237
+    HeaderH.beginFill(0x210237, 0.9);//0x650A5A 0x210237
     HeaderH.drawRoundedRect(60, 83, 1159, 554, 14);
     HeaderH.endFill();
-
-    const startLobby = this.container.getChildByName("Start");
-    startLobby.visible = false;
+    DataGrid .addChild(HeaderH);
+    
     // Rectangle
     const style = new PIXI.TextStyle({fill:'#f6ba32',fontWeight: 'bold',fontSize: 14});
-    const style1 = new PIXI.TextStyle({fill:'#ffffff',fontWeight: 'bold',fontSize: 13});
+    //const style1 = new PIXI.TextStyle({fill:'#ffffff',fontWeight: 'bold',fontSize: 13});
     const style2 = new PIXI.TextStyle({fill:'#ffffff',fontWeight: 'bold',fontSize: 24});
-
-    const MyAccount = new PIXI.Text('My Account', style2);
+    var arrHeaderXpos:number[] = [315, 555, 797, 1064];
+    for(var j:number = 0; j < arrHeader.length; ++j)
+    {
+      console.log("arrHeader[j] = " + arrHeader[j]);
+      const txtHeader = new PIXI.Text(arrHeader[j], style2);
+      txtHeader.x = arrHeaderXpos[j];
+      txtHeader.y = 100;
+      DataGrid .addChild(txtHeader);
+    }
+    DataGrid .addChild(Header);
+    /*const MyAccount = new PIXI.Text('My Account', style2);
     MyAccount.x = 315;
     MyAccount.y = 98;
 
@@ -218,9 +273,17 @@ import { Games } from './../entity/Games';
 
     const RPS = new PIXI.Text('RPS', style2);
     RPS.x = 364;
-    RPS.y = 100;
+    RPS.y = 100;*/
+    var arrXpos:number[] = [100, 243, 364,532, 677,773,941,1109];
+    for(var i:number = 0; i < arrDataGridHeader.length; ++i)
+    {
+      const text = new PIXI.Text(arrDataGridHeader[i], style);
+      text.x = arrXpos[i];
+      text.y = 180;
+      DataGrid .addChild(text);
+    }
 
-    const Name = new PIXI.Text('Name', style);
+    /*const Name = new PIXI.Text('Name', style);
     Name.x = 100;
     Name.y = 180;
 
@@ -250,96 +313,176 @@ import { Games } from './../entity/Games';
 
     const Action = new PIXI.Text('Action', style);
     Action.x = 1109;
-    Action.y = 180;
+    Action.y = 180;*/
     
-    DataGrid.addChild(HeaderH);
-    DataGrid.addChild(MyAccount);
-    DataGrid.addChild(Promotions);
-    DataGrid.addChild(BringAFriend);
-    DataGrid.addChild(Header);
-    DataGrid.addChild(Name);
+   
+    // DataGrid.addChild(MyAccount);
+    // DataGrid.addChild(Promotions);
+    // DataGrid.addChild(BringAFriend);
+   
+    /*DataGrid.addChild(Name);
     DataGrid.addChild(Deal);
     DataGrid.addChild(MaxPlayers);
     DataGrid.addChild(EntryFee);
     DataGrid.addChild(Prize);
     DataGrid.addChild(ActivePlayers);
     DataGrid.addChild(Registering);
-    DataGrid.addChild(Action);
-    const lobbyspr = new PIXI.Sprite(this.lobbytable);
-    this.RenderData(param, DataGrid);
-    this.container.addChild(lobbyspr);
+    DataGrid.addChild(Action);*/
+    //const lobbyspr = new PIXI.Sprite(this.lobbytable);
+    //this.container.addChild(lobbyspr);
+    this.home = PIXI.Texture.from("./../src/Assets/img/cards.png");
+    const homespr = new PIXI.Sprite(this.home);
+    homespr.name = "home";
+    homespr.x = 500;
+    homespr.y = 20;
+    homespr.scale.x = 0.10;
+    homespr.scale.y = 0.10;
+    homespr.interactive = true;        
+    homespr.buttonMode = true;
+    homespr.on('pointerdown', this.MoveBack, this);
+    this.RenderData(param,  DataGrid , strType);
+    this.container.addChild(homespr);
     this.container.addChild(DataGrid);
+    this.DataGrid = DataGrid;
     
   }
 
-  private RenderData(param: [Games], DataGrid)
+  private MoveBack(event)
+  {
+    const startLobby = this.container.getChildByName("Start");
+    startLobby.visible = true;
+    this.DataGrid.visible = false;
+    const home = this.container.getChildByName("home");
+    home.visible = false;
+   
+  }
+
+  private RenderData(param: [Games], DataGrid, strType:string)
   {
     console.log("onClick param.length  = " + param.length);
     const lobbyGrid = new PIXI.Container();
     const style1 = new PIXI.TextStyle({fill:'#ffffff',fontWeight: 'bold',fontSize: 13});
+    var arrXpos:number[] = [40, 200, 350,485, 625,755,920,1050];
+    const Mask = new PIXI.Graphics();
+    Mask.beginFill(0x210237, 0.5);//0x650A5A 0x210237
+    Mask.drawRoundedRect(60, 83, 1159, 554, 14);
+    Mask.endFill();
+    const scroll = new PIXI.Graphics();
+    scroll.beginFill(0xcccccc, 0.2);//0x650A5A 0x210237
+    scroll.drawRect(1202, 241, 2, 395);
+    scroll.endFill();
+    const scrollbar = new PIXI.Graphics();
+    scrollbar.beginFill(0xcccccc, 0.8);//0x650A5A 0x210237
+    scrollbar.drawRoundedRect(1198, 241, 10, 40, 1);
+    scrollbar.endFill();
+    scrollbar.interactive = true;
+    scrollbar.buttonMode = true;
+    scrollbar
+        .on('pointerdown', this.onDragStart, this)
+        .on('pointerup', this.onDragEnd, this)
+        .on('pointerupoutside', this.onDragEnd, this)
+        .on('pointermove', this.onDragMove, this);
+    lobbyGrid.mask = Mask;
+    var arrText:string[];
     for(var i:number = 0; i < param.length; ++i)
     {
         const lobbyspr = new PIXI.Sprite();
-        //console.log("onClick param  = " + param[i]);
+        
+        if(strType == "Deals")
+        {
+          arrText = [param[i]["game_title"], param[i]["deals"].toString(), 
+          param[i]["seats"].toString(),param[i]["entry_fee"].toString(), 
+          param[i]["pool_deal_prize"].toString(),"0","0"];
+        }
+        else if(strType == "Pool"){
+          arrText = [param[i]["game_title"], param[i]["pool_game_type"].toString(), 
+          param[i]["seats"].toString(),param[i]["entry_fee"].toString(), 
+          param[i]["pool_deal_prize"].toString(),"0","0"];
+        }
+        else{
+          arrText = [param[i]["game_title"], param[i]["number_of_deck"].toString(), 
+          param[i]["seats"].toString(),param[i]["entry_fee"].toString(), 
+          param[i]["pool_deal_prize"].toString(),"0","0"];
+        }
+       
+        console.log("onClick param  = " + param[i]);
         const Header = new PIXI.Graphics();
         lobbyspr.y = i * 51;
-        const name = new PIXI.Text(param[i].game_title, style1);
-        name.x = 25;
-        name.y = 20;
-
-        const deals = new PIXI.Text(param[i].deals.toString(), style1);
-        deals.x = 200;
-        deals.y = 20;
-
-        const maxPlayers = new PIXI.Text(param[i].seats.toString(), style1);
-        maxPlayers.x = 350;
-        maxPlayers.y = 20;
-
-        const entryFee = new PIXI.Text(param[i].entry_fee.toString(), style1);
-        entryFee.x = 485;
-        entryFee.y = 20;
-
-        const prize = new PIXI.Text(param[i].pool_deal_prize.toString(), style1);
-        prize.x = 625;
-        prize.y = 20;
-
-        const activePlayers = new PIXI.Text("0", style1);
-        activePlayers.x = 755;
-        activePlayers.y = 20;
-        const Registering = new PIXI.Text("0", style1);
-        Registering.x = 920;
-        Registering.y = 20;
-
+        lobbyspr.addChild(Header);
+        for(var j:number = 0; j < arrText.length; ++j)
+        {
+          const text = new PIXI.Text(arrText[j], style1);
+          text.x = arrXpos[j];
+          text.y = 20;
+          lobbyspr.addChild(text);
+        }
         var playNow = PIXI.Texture.from("./../src/Assets/img/play-now.png");
         const playNowspr = new PIXI.Sprite(playNow);
         playNowspr.x = 1050;
-        playNowspr.y = 13;
+        playNowspr.y = 15;
         playNowspr.scale.x = 0.45;
         playNowspr.scale.y = 0.45;
+        playNowspr.interactive = true;
+        
+        playNowspr.buttonMode = true;
+        var oToken:Object = new Object();
+        oToken["token"] = param[i]["token"]
+        playNowspr.on('pointerdown', this.openGame, oToken);
         
         Header.beginFill(0x0c0015,((i%2) * 1));//(0x0c0015);(0xDE3249) ((i%2) * 1)
         Header.drawRect(0, 0, 1159, 51);
         Header.endFill();
         Header.lineStyle(2, 0xFF00FF, 1);
 
-        lobbyspr.addChild(Header);
-        lobbyspr.addChild(name);
-        lobbyspr.addChild(deals);
-        lobbyspr.addChild(maxPlayers);
-        lobbyspr.addChild(entryFee);
-        lobbyspr.addChild(prize);
-        lobbyspr.addChild(activePlayers);
-        lobbyspr.addChild(Registering);
         lobbyspr.addChild(playNowspr);
         lobbyGrid.addChild(lobbyspr);
     }
     lobbyGrid.x = 60;
     lobbyGrid.y = 239;
-    DataGrid.addChild(lobbyGrid);
+    //DataGrid.addChild(scroll);
+    DataGrid.addChild(Mask, lobbyGrid);
+    DataGrid.addChild(scroll);
+    DataGrid.addChild(scrollbar);
     /*
     console.log("onClick (i)  = " + (i));
         console.log("onClick (i%2)  = " + (i%2));
         console.log("onClick (i%2)*1  = " + (i%2) * 1);*/
+  }
+  private onDragStart(event)
+  {
+    console.log("openGame event.target  = " + event.target);
+    console.log("openGame event.currentTarget  = " + event.currentTarget);
+    event.currentTarget.dragging = true;
+    //this.data = event.data;
+    //this.alpha = 0.5;
+    //this.dragging = true;
+  }
+  private onDragEnd(event)
+  {
+    event.currentTarget.dragging = false;
+    //this.dragging = true;
+  }
+  private onDragMove(event)
+  {
+    if (event.currentTarget.dragging) {
+      const newPosition = event.currentTarget.getLocalPosition(event.currentTarget.parent);
+      event.currentTarget.x = newPosition.x;
+      event.currentTarget.y = newPosition.y;
+  }
+  }
+ 
+
+  private openGame(event)
+  {
+    console.log("openGame event  = " + event);
+    console.log("openGame this  = " + this);
+    console.log("openGame getOwnPropertyNames this  = " + Object.getOwnPropertyNames(this));
+    console.log("openGame getOwnPropertyNames token  = " + this["token"]);
+    console.log("openGame getOwnPropertyNames event = " + Object.getOwnPropertyNames(event));
+    //window.open("https://rummydesk.com/", );
+    //global.game.OpenWindow("https://rummydesk.com/index.php/game_lobby/" + this.nOpenGame);
+    global.game.OpenWindow("http://127.0.0.1:5500/" + this);
+    //this.nOpenGame++;
   }
 }
 
